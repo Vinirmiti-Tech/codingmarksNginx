e2ffd1364d1b66de1ce0ce90ad2dcaa1ff6f2f4b
Vinirmiti-Tech <32204640+Vinirmiti-Tech@users.noreply.github.com>
2019-03-01 00:55:31 +0530

    Update install_docker.sh

--- nextcloud/install_docker.sh
+++ nextcloud/install_docker.sh
@@ -22,4 +22,11 @@ sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/debia
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+sudo docker --version
 
+########## install compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
