ec77f7121f17e132a49e43041e7f0ced10c8706b
Vinirmiti-Tech <32204640+Vinirmiti-Tech@users.noreply.github.com>
2019-03-01 01:07:09 +0530

    Update installNextCloud.sh

--- nextcloud/installNextCloud.sh
+++ nextcloud/installNextCloud.sh
@@ -6,6 +6,8 @@ export MYSQL_PASSWORD=#get20
 export NEXTCLOUD_ADMIN_USER=admin
 export NEXTCLOUD_ADMIN_PASSWORD=#getAdmin20
 
+echo echoing values
+echo $NEXTCLOUD_ADMIN_USER
 # assuming you have installed docker and docker compose
 
 docker-compose up -d
