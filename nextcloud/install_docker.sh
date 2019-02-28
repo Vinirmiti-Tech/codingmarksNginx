ac767ab7d85a1af0289a586be3722a1a63a4236f
Vinirmiti-Tech <32204640+Vinirmiti-Tech@users.noreply.github.com>
2019-03-01 00:51:52 +0530

    Create install_docker.sh

---
+++ nextcloud/install_docker.sh
@@ -0,0 +1,25 @@
+# Debian
+
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Update the apt package index
+sudo apt-get update
+
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+# Verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88, 
+# by searching for the last 8 characters of the fingerprint.
+sudo apt-key fingerprint 0EBFCD88
+
+# set up the stable repository
+sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+
